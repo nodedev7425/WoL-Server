f5996b0df64d1cac37364eec32c82be074d804da
nodedev7425 <buelenard@tutanota.com>
2026-04-26 14:42:53 +0200

    Implement timezone and language code correctly

--- app/settings.py
+++ app/settings.py
@@ -106,12 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = env('LANGUAGE_CODE')
+TIME_ZONE = env('TIME_ZONE')
 USE_I18N = True
-
 USE_TZ = True
 
 
