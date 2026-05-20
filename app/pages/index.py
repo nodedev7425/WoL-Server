941bfe8239afe115d49a9ae715046441b23dbc02
nodedev7425 <buelenard@tutanota.com>
2026-05-20 22:08:36 +0200

    Remove pages module and use View classes

--- app/pages/index.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from django.contrib.auth.decorators import login_required
-from django.views.decorators.csrf import csrf_protect
-
-@csrf_protect
-@login_required(login_url='/login')
-def index_view(request):
-    return render(request, 'index.html')
--- app/views.py
+++ app/views.py
@@ -25,4 +25,20 @@ class LoginView(View):
 
     @csrf_protect
     def get(self, request, *args, **kwargs):
-        return render(request, "login.html") 
+
+        if request.user.is_authenticated:
+            return redirect(request.POST['next'])
+
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(request.POST['next'])
+            else:
+                return JsonResponse({'error': 'Authentication failed'}, status=401)
+
+        next_url = request.GET.get('next', '')
+        return render(request, 'login.html', { 'next': next_url })
